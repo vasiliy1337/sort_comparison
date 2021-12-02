c9d7ca4648fc96121929abb14d2011d7fc3c3157
dashayurovskaya <dashayurovskaya89@gmail.com>
2021-12-02 20:22:32 +0300

    добавление mergesort
    сортировка слиянием

---
+++ sort_comparison/MergeSortDasha/MergeSortDasha/mergesort.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+void merge_sort(vector <T>& data, size_t start, size_t end)
+{
+	if ((end - start) < 2)
+		return;
+	if ((end - start) == 2)
+	{
+		if (data[start] > data[start + 1])
+			swap(data[start], data[start + 1]);
+		return;
+	}
+	merge_sort(data, start, start + (end - start) / 2);
+	merge_sort(data, start + (end - start) / 2, end);
+	vector<int> b;
+	size_t start_b1 = start;
+	size_t end_e1 = start + (end - start) / 2;
+	size_t start_b2 = end_e1;
+	size_t end_b2 = end;
+	while (b.size() < end - start)
+	{
+		if (start_b1>= end_e1 || (start_b2 < end && data[start_b2] <= data[start_b1]))
+		{
+			b.push_back(data[start_b2]);
+			start_b2++;
+		}
+		else
+		{
+			b.push_back(data[start_b1]);
+			start_b1++;
+		}
+	}
+	for (size_t i = start; i < end; i++)
+		data[i] = b[i - start];
+}
+
+vector<int> merge_sort_yurovskaya_int(vector<int> data)
+{
+	vector <int> b(data.size());
+	merge_sort(data, 0, data.size() - 1);
+	return data;
+}
+
+vector<double> merge_sort_yurovskaya_double(vector<double> data)
+{
+	vector <double> b(data.size());
+	merge_sort(data, 0, data.size() - 1);
+	return data;
+}
